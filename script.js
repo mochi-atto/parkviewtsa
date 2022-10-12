1b1c3329dfdc3366bf604552acd6e33f4d500684
mochi-atto <101821714+mochi-atto@users.noreply.github.com>
2022-10-12 10:26:12 -0400

    bunch of stuff

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ document.getElementById("button").onclick = toggleVisibility();
 function toggleVisibility(){
   document.getElementById("content").classList.toggle("visibility")
 }
+
+var clientHeight = document.getElementById('container').clientHeight;
